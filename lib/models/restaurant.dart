f063b5a4810dfcb16010440af9b1827c17492182
Jiaduobao26 <mitsuitoto@gmail.com>
2025-05-27 14:51:54 -0700

    extend Restaurant class with additional fields

--- lib/models/restaurant.dart
+++ lib/models/restaurant.dart
@@ -1,15 +1,30 @@
-class Restaurant {
+import 'package:equatable/equatable.dart';
+
+class Restaurant extends Equatable {
   final String name;
   final String cuisine;
   final double rating;
   final String address;
   final String imageUrl;
 
-  Restaurant({
+  const Restaurant({
     required this.name,
     required this.cuisine,
     required this.rating,
     required this.address,
     required this.imageUrl,
   });
-} 
+
+  factory Restaurant.fromJson(Map<String, dynamic> json) {
+    return Restaurant(
+      name: json['name'],
+      cuisine: json['cuisine'],
+      rating: (json['rating'] as num).toDouble(),
+      address: json['address'],
+      imageUrl: json['imageUrl'],
+    );
+  }
+
+  @override
+  List<Object?> get props => [name, cuisine, rating, address, imageUrl];
+}
